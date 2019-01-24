f624a3a018a8011a72e711c99952b1c413f700e4
Jason Etcovitch <jasonetco@github.com>
2019-01-24 11:33:21 -0500

    Write get-repos

---
+++ functions/get-repos.js
@@ -0,0 +1,34 @@
+const gql = require('@octokit/graphql')
+
+const query = `{ 
+  viewer {
+    repositories (affiliations: OWNER, first: 2, orderBy: { field: PUSHED_AT, direction: DESC }, isFork: false) {
+      nodes {
+        name
+        description
+        stargazers {
+          totalCount
+        }
+        forkCount
+        primaryLanguage {
+          color
+          name
+        }
+      }
+    }
+  }
+}`
+
+export async function handler() {
+  const res = await gql(query, {
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`
+    }
+  })
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(res.viewer.repositories.nodes)
+  }
+}
