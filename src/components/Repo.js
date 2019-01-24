98d609cdab7ab9e9ec5785a448359d6f3c30a84c
Jason Etcovitch <jasonetco@github.com>
2019-01-24 17:39:11 -0500

    Typo

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -150,7 +150,7 @@ export default function Repo(props) {
                 d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
               />
             </svg>
-            {repo.forksCount}
+            {repo.forkCount}
           </a>
         </p>
       </div>
@@ -163,7 +163,7 @@ Repo.propTypes = {
   repo: shape({
     url: string.isRequired,
     name: string.isRequired,
-    forksCount: number.isRequired,
+    forkCount: number.isRequired,
     stargazers: shape({
       totalCount: number.isRequired
     }),
