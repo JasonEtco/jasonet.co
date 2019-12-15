213d177cead85eec49074409123b0acc1c2a7db9
Jason Etcovitch <jasonetco@github.com>
2019-12-15 16:53:45 -0500

    Restyle the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Bio from '../components/Bio'
 import Seo from '../components/SEO'
 import ReposList from '../components/ReposList'
 import { formatReadingTime } from '../utils/helpers'
-import { rhythm, scale } from '../utils/typography'
 import { shape, arrayOf } from 'prop-types'
 import p from '../utils/shared-props'
 
@@ -13,33 +12,16 @@ export default function BlogIndex(props) {
   const posts = props.data.allMarkdownRemark.edges
 
   return (
-    <main
-      style={{
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(1)}`
-      }}
-    >
+    <main className="mx-auto max-w-xl py-8 px-3">
       <Seo />
 
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          borderBottom: '1px solid hsla(0,0%,0%,0.07)',
-          paddingBottom: rhythm(0.5)
-        }}
-        className="home__hero"
-      >
+      <header className="md:flex items-end border-b border-gray-200 pb-3 mb-10">
         <h1
+          className="border-0 mb-0 pb-0"
           style={{
-            ...scale(1.8),
+            fontSize: '3.5rem',
             fontWeight: 900,
-            lineHeight: 0.9,
-            border: 'none',
-            margin: 0,
-            padding: 0
+            lineHeight: 0.9
           }}
         >
           Jason
@@ -47,21 +29,30 @@ export default function BlogIndex(props) {
           Etcovitch
         </h1>
 
-        <p style={{ alignSelf: 'flex-end', marginBottom: 0, marginLeft: 16 }}>
-          🐙 Engineer at <a href="https://github.com/JasonEtco">GitHub</a>
-          <br />
-          🐦 Follow me on <a href="https://twitter.com/JasonEtco">Twitter</a>
-        </p>
-      </div>
+        <div className="flex-end mt-6 mb-0 md:mt-0 md:ml-3">
+          <p className="mb-1">
+            🐙 Engineer at{' '}
+            <a
+              className="text-orange hover:underline"
+              href="https://github.com/JasonEtco"
+            >
+              GitHub
+            </a>
+          </p>
+          <p>
+            🐦 Follow me on{' '}
+            <a
+              className="text-orange hover:underline"
+              href="https://twitter.com/JasonEtco"
+            >
+              Twitter
+            </a>
+          </p>
+        </div>
+      </header>
 
-      <div
-        style={{
-          paddingTop: rhythm(2),
-          paddingBottom: rhythm(2),
-          borderBottom: '1px solid hsla(0,0%,0%,0.07)'
-        }}
-      >
-        <h5>Posts:</h5>
+      <div className="py-6 border-b border-gray-200">
+        <h5 className="text-sm text-gray-700 font-semibold">Posts:</h5>
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
@@ -69,40 +60,34 @@ export default function BlogIndex(props) {
               key={node.fields.slug}
               itemScope
               itemType="https://schema.org/BlogPosting"
+              className="mt-8"
             >
-              <h3 itemProp="name" style={{ marginBottom: rhythm(1 / 4) }}>
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
+              <h3
+                itemProp="name"
+                className="mb-1 text-orange text-2xl font-semibold"
+              >
+                <Link to={node.fields.slug}>{title}</Link>
               </h3>
-              <small>
+              <small className="text-gray-700">
                 <time itemProp="datePublished">{node.frontmatter.date}</time>
                 <span> • </span>
                 <time itemProp="timeRequired">
                   {formatReadingTime(node.timeToRead)}
                 </time>
               </small>
-              <p
-                itemProp="about headline comment"
-                dangerouslySetInnerHTML={{ __html: node.frontmatter.spoiler }}
-              />
+              <p itemProp="about headline comment">
+                {node.frontmatter.spoiler}
+              </p>
             </div>
           )
         })}
       </div>
 
-      <div style={{ marginTop: rhythm(3) }}>
+      <div className="mt-20">
         <ReposList />
       </div>
 
-      <div
-        style={{
-          padding: rhythm(1),
-          marginTop: rhythm(2),
-          boxShadow: '0 2px 15px 0 rgba(210,214,220,.5)',
-          borderRadius: 5
-        }}
-      >
+      <div className="p-8 mt-10 shadow-lg border border-gray-100 rounded-lg">
         <Bio marginBottom={0} />
       </div>
     </main>
