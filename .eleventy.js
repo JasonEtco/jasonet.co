258a973a4e89f925d88ef09a4b45c9614f7572de
Jason Etcovitch <jasonetco@github.com>
2021-12-25 22:59:43 +0000

    rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
 const timeToRead = require('eleventy-plugin-time-to-read')
 const tableOfContents = require('eleventy-plugin-toc')
 const dateFilter = require('nunjucks-date-filter')
 
 module.exports = (eleventyConfig) => {
   dateFilter.setDefaultFormat('MMMM D, YYYY')
-
+  
+  eleventyConfig.addPlugin(pluginRss)
   eleventyConfig.addPlugin(tableOfContents)
   eleventyConfig.addPlugin(syntaxHighlight)
   eleventyConfig.addPlugin(timeToRead)
