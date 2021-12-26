38cd77d8ddbe179021cf808eeff612359bc5cf15
Jason Etcovitch <jasonetco@github.com>
2021-12-26 02:12:32 +0000

    Get the get-repos function working

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const pluginRss = require('@11ty/eleventy-plugin-rss')
 const timeToRead = require('eleventy-plugin-time-to-read')
 const tableOfContents = require('eleventy-plugin-toc')
 const dateFilter = require('nunjucks-date-filter')
+const octicons = require('@primer/octicons')
 
 module.exports = (eleventyConfig) => {
   dateFilter.setDefaultFormat('MMMM D, YYYY')
@@ -12,6 +13,10 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(syntaxHighlight)
   eleventyConfig.addPlugin(timeToRead)
   eleventyConfig.addNunjucksFilter('date', dateFilter)
-
   eleventyConfig.addPassthroughCopy('assets')
+
+  eleventyConfig.addShortcode('octicon', (name) => {
+    const result = octicons[name].toSVG()
+    return result
+  })
 }
