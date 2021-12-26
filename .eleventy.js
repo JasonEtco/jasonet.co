55e0ea3695b1caf5be3103941c4eafe1eaa0ed28
Jason Etcovitch <jasonetco@github.com>
2021-12-26 02:37:42 +0000

    Cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -27,44 +27,32 @@ module.exports = (eleventyConfig) => {
     class: "anchor",
     symbol: "<span hidden>#</span>",
     style: "aria-labelledby",
-  });
+  })
+
   const markdownItAnchorOptions = {
     level: [1, 2, 3],
-    slugify: (str) =>
-      slugify(str, {
-        lower: true,
-        strict: true,
-        remove: /["]/g,
-      }),
+    slugify: (str) => slugify(str, { lower: true, strict: true, remove: /["]/g, }),
     tabIndex: false,
     permalink(slug, opts, state, idx) {
-      state.tokens.splice(
-        idx,
-        0,
-        Object.assign(new state.Token("div_open", "div", 1), {
+      state.tokens.splice(idx, 0, Object.assign(new state.Token("div_open", "div", 1), {
           // Add class "header-wrapper [h1 or h2 or h3]"
           attrs: [["class", `heading-wrapper ${state.tokens[idx].tag}`]],
           block: true,
         })
-      );
+      )
   
-      state.tokens.splice(
-        idx + 4,
-        0,
-        Object.assign(new state.Token("div_close", "div", -1), {
+      state.tokens.splice(idx + 4, 0, Object.assign(new state.Token("div_close", "div", -1), {
           block: true,
         })
-      );
+      )
   
-      linkAfterHeader(slug, opts, state, idx + 1);
+      linkAfterHeader(slug, opts, state, idx + 1)
     },
-  };
+  }
   
   /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-  }).use(markdownItAnchor, markdownItAnchorOptions);
+  const markdownLibrary = markdownIt({ html: true }).use(markdownItAnchor, markdownItAnchorOptions)
   
   // This is the part that tells 11ty to swap to our custom config
-  eleventyConfig.setLibrary("md", markdownLibrary);
+  eleventyConfig.setLibrary("md", markdownLibrary)
 }
